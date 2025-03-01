83522865e49b3ad4a5acf1e3b03589611b95154a
PJ Gray <pj@evertrue.com>
2025-03-01 10:16:14 -0500

    update to 1.5

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
         .package(url: "https://github.com/swiftpackages/DotEnv.git", from: "3.0.0")
     ],
     targets: [
